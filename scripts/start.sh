87bbcada680ce335a86cf0b0b6011b8a2b647b4f
Lewis Wright <LewisW@users.noreply.github.com>
2015-03-02 20:30:49 +0000

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,7 +7,6 @@ xvfb $DISPLAY -shmem -ac -screen 0 1024x768x16 > build/logs/xvfb.log 2>&1 &
 if [ "$SCREEN" == "VNC" ] || [ "$SCREEN" == "Record VNC" ]; then
   echo -e "\e[1;4mLaunching VNC Server\e[0m"
   
-  #vncserver $DISPLAY -localhost -securitytypes=none
   x11vnc -localhost -nopw -display $DISPLAY -N -forever & > build/logs/vnc.log 2>&1 &
 
   if [ "$SCREEN" == "VNC & Record" ]; then
@@ -17,11 +16,11 @@ if [ "$SCREEN" == "VNC" ] || [ "$SCREEN" == "Record VNC" ]; then
   
 fi
 
-if [ "$Selenium" == "Selenium" ]; then
+if [ "$SELENIUM" == "PhantomJS" ]; then
+    echo -e "\e[1;4mLaunching PhantomJS\e[0m"
+    phantomjs --webdriver=4444 > build/logs/phantomjs.log 2>&1 &
+else
     echo -e "\e[1;4mLaunching Selenium\e[0m"
     # selenium must be started by a non-root user otherwise chrome can't start
     su - seleuser -c "selenium-standalone start > build/logs/selenium.log 2>&1 &"
-elif [ "$Selenium" == "PhantomJS" ]; then
-    echo -e "\e[1;4mLaunching PhantomJS\e[0m"
-    phantomjs --webdriver=4444 > build/logs/phantomjs.log 2>&1 &
 fi
