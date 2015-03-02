f9d1466e04fff18113e684274f1c964323c9103d
Lewis Wright <LewisW@users.noreply.github.com>
2015-03-02 20:31:21 +0000

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,19 +1,17 @@
 mkdir -p build/logs
 export DISPLAY=:99
 
-echo -e "\e[1;4mLaunching Virtual Screen\e[0m"
-xvfb $DISPLAY -shmem -ac -screen 0 1024x768x16 > build/logs/xvfb.log 2>&1 &
-
 if [ "$SCREEN" == "VNC" ] || [ "$SCREEN" == "Record VNC" ]; then
   echo -e "\e[1;4mLaunching VNC Server\e[0m"
-  
   vncserver $DISPLAY -localhost -securitytypes=none
   
   if [ "$SCREEN" == "VNC & Record" ]; then
     echo -e "\e[1;4mLaunching VNC Recorder\e[0m"
     flvrec.py -o build/vnc.flv localhost$DISPLAY > build/logs/flvrec.log 2>&1 &
   fi
-  
+else
+  echo -e "\e[1;4mLaunching Virtual Screen\e[0m"
+  xvfb $DISPLAY -shmem -ac -screen 0 1024x768x16 > build/logs/xvfb.log 2>&1 &
 fi
 
 if [ "$SELENIUM" == "PhantomJS" ]; then
