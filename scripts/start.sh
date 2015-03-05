25c27e4af256a1824c3ce2ae003322cce8a03c22
Lewis Wright <LewisW@users.noreply.github.com>
2015-03-05 14:54:03 +0000

    Fixed some small bugs

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,26 +2,29 @@
 
 export DISPLAY=:99
 
+mkdir -p /build
+chmod -R 777 /build
+
 echo -e "\e[1;4mLaunching Virtual Screen\e[0m"
-Xvfb $DISPLAY -shmem -ac -screen 0 1024x768x16 > xvfb.log 2>&1 &
+Xvfb $DISPLAY -shmem -ac -screen 0 1024x768x16 > /build/xvfb.log 2>&1 &
 
 if [ "$SCREEN" == "VNC" ] || [ "$SCREEN" == "Record VNC" ]; then
   echo -e "\e[1;4mLaunching VNC Server\e[0m"
   
-  x11vnc -localhost -nopw -display $DISPLAY -N -forever & > vnc.log 2>&1 &
+  x11vnc -localhost -nopw -display $DISPLAY -N -forever > /build/vnc.log 2>&1 &
 
-  if [ "$SCREEN" == "VNC & Record" ]; then
+  if [ "$SCREEN" == "Record VNC" ]; then
     echo -e "\e[1;4mLaunching VNC Recorder\e[0m"
-    flvrec.py -o build/vnc.flv localhost$DISPLAY > flvrec.log 2>&1 &
+    flvrec.py -o build/vnc.flv localhost$DISPLAY > /build/flvrec.log 2>&1 &
   fi
   
 fi
 
 if [ "$SELENIUM" == "PhantomJS" ]; then
     echo -e "\e[1;4mLaunching PhantomJS\e[0m"
-    phantomjs --webdriver=4444 > phantomjs.log 2>&1
+    phantomjs --webdriver=4444 > /build/phantomjs.log 2>&1
 else
     echo -e "\e[1;4mLaunching Selenium\e[0m"
     # selenium must be started by a non-root user otherwise chrome can't start
-    su - seleuser -c "selenium-standalone start > selenium.log 2>&1"
+    su - seleuser -c "selenium-standalone start > /build/selenium.log 2>&1"
 fi
